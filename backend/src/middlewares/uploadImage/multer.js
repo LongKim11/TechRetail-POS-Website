d859d2280a8015fb8050ddae20927ba30c2c5a1f
Vo Kim Long <52200226@student.tdtu.edu.vn>
2024-10-19 13:33:18 +0700

    Remove redundant files

--- backend/src/middlewares/uploadImage/multer.js
+++ backend/src/middlewares/uploadImage/multer.js
@@ -1,65 +1,73 @@
-import multer from "multer";
+import fs from 'fs'
+import multer from 'multer'
+import path from 'path'
 
 const avatarStorage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, __dirname + "/uploads/avatars");
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(
-      null,
-      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
-    );
-  },
-});
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads/avatars')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
+        cb(
+            null,
+            file.fieldname +
+                '-' +
+                uniqueSuffix +
+                path.extname(file.originalname),
+        )
+    },
+})
 
 const productImgStorage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, __dirname + "/uploads/products");
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(
-      null,
-      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
-    );
-  },
-});
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads/products')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
+        cb(
+            null,
+            file.fieldname +
+                '-' +
+                uniqueSuffix +
+                path.extname(file.originalname),
+        )
+    },
+})
 
 const uploadAvatar = multer({
-  storage: avatarStorage,
-  limits: {
-    fileSize: 1024 * 1024 * 5,
-  },
-  fileFilter: (req, file, cb) => {
-    if (
-      file.mimetype === "image/jpeg" ||
-      file.mimetype === "image/png" ||
-      file.mimetype === "image/jpg"
-    ) {
-      cb(null, true);
-    } else {
-      cb(new Error("Invalid file type"));
-    }
-  },
-});
+    storage: avatarStorage,
+    limits: {
+        fileSize: 1024 * 1024 * 5,
+    },
+    fileFilter: (req, file, cb) => {
+        if (
+            file.mimetype === 'image/jpeg' ||
+            file.mimetype === 'image/png' ||
+            file.mimetype === 'image/jpg'
+        ) {
+            cb(null, true)
+        } else {
+            cb(new Error('Invalid file type'))
+        }
+    },
+})
 
 const uploadProductImg = multer({
-  storage: productImgStorage,
-  limits: {
-    fileSize: 1024 * 1024 * 5,
-  },
-  fileFilter: (req, file, cb) => {
-    if (
-      file.mimetype === "image/jpeg" ||
-      file.mimetype === "image/png" ||
-      file.mimetype === "image/jpg"
-    ) {
-      cb(null, true);
-    } else {
-      cb(new Error("Invalid file type"));
-    }
-  },
-});
+    storage: productImgStorage,
+    limits: {
+        fileSize: 1024 * 1024 * 5,
+    },
+    fileFilter: (req, file, cb) => {
+        if (
+            file.mimetype === 'image/jpeg' ||
+            file.mimetype === 'image/png' ||
+            file.mimetype === 'image/jpg'
+        ) {
+            cb(null, true)
+        } else {
+            cb(new Error('Invalid file type'))
+        }
+    },
+})
 
-export { uploadAvatar, uploadProductImg };
+export { uploadAvatar, uploadProductImg }

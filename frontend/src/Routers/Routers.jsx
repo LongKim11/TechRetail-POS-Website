c3bbf3ed8965f36b69278965d1d4794a1b0f44bc
Long Vo <vokimlong11.cs@gmail.com>
2024-10-29 13:15:56 +0700

    Add sidebar and box shadow for login form

--- frontend/src/Routers/Routers.jsx
+++ frontend/src/Routers/Routers.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/Login/LoginPage";
+import Home from "../pages/Home/Home";
 
 const Routers = () => {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 };
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,14 @@
+import Sidebar from "../../layouts/Sidebar/sidebar";
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="h-screen flex-1 p-7">
+        <h1 className="text-2xl font-semibold ">Home Page</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Home;

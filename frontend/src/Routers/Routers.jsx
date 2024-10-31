bd3ccebeba7c0b0fbe0e9ae8ad75452ea05580c1
Vo Kim Long <52200226@student.tdtu.edu.vn>
2024-10-31 15:57:01 +0700

    Add profile page

--- frontend/src/Routers/Routers.jsx
+++ frontend/src/Routers/Routers.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/Login/LoginPage";
 import Home from "../pages/Home/Home";
+import Profile from "../pages/Profile/Profile";
 
 const Routers = () => {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/" element={<Home />} />
+      <Route path="/profile" element={<Profile />}></Route>
     </Routes>
   );
 };
